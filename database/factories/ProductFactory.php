1f7b7cd8e4588dc86d04ec4ede0b4e86d17dac1e
Kevin Manceau <kevin.manceau@gmail.com>
2024-12-11 09:47:52 +0100

    Product seeder

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->words(2, true),
+//            'description' => fake()->words(5),
+            'description' => fake()->words(25, true),
+            'baseprice' => fake()->randomFloat(2, 1, 100000000),
+//            'baseprice' => fake()->randomNumber(),
+            'category_id' => rand(1, Category::count()),
+        ];
+    }
+}
